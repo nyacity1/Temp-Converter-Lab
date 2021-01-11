f73f488fe6ffeeb56e2fb39d5e4001c4713fd935
nyacity1 <74576472+nyacity1@users.noreply.github.com>
2021-01-10 20:12:55 -0500

    myConverter

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-  function yourFunctionName() {
-  //The start of your function 
-    let celsiusCalc = parseFloat(document.getElementById("degrees").value);
+  function myConverter() {
+  const degreeContainer=document.getElementById("degrees").value 
+  const calc=(degreeContainer - 32) *5/9;
+  document.getElementById("answer").innerHTML=calc;
+  
+    //let celsiusCalc = parseFloat(document.getElementById("degrees").value);
      //degrees is the value inside the input box
-    document.getElementById("getCelsius").innerHTML = Math.round(         
+   // document.getElementById("getCelsius").innerHTML = Math.round(         
    //getCelsius the where it would put result of calculating
-      celsiusCalc * (9 / 5) + 32
-    );
+   //   celsiusCalc * (9 / 5) + 32
+  
   }
-
+  
+  
 
   
